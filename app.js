2b997e753e8d78b72574e0013a83dc557faede84
vaishnavisadhu1tecblic-spec <vaishnavisadhu1.tecblic@gmail.com>
2026-09-09 12:26:21 +0530

    Updated code

--- app.js
+++ app.js
@@ -25,9 +25,11 @@ connectDB();
 // MIDDLEWARE
 // =====================================================
 
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: allowedOrigins,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
@@ -68,7 +70,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: allowedOrigins,
     methods: ["GET", "POST"],
     credentials: true,
   },
